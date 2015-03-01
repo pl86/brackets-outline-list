bb18c743dd9d91861515881fdea623a72abd4888
Piotr Labudda <piotrl86@gmail.com>
2015-03-01 21:44:24 +0100

    Modify getOutlineList for PHP and add class name (fixes #24)

--- src/languages/PHP.js
+++ src/languages/PHP.js
@@ -3,15 +3,15 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var defaultVisibilty = "public";
-    var unnamedPlaceholder = "function";
+    var defaultVisibilty = "public",
+        unnamedPlaceholder = "function";
 
     function createListEntry(name, args, vis, line, ch) {
-        var $elements = [];
-        var $name = $(document.createElement("span"));
+        var $elements = [],
+            $name = $(document.createElement("span")),
+            $arguments = $(document.createElement("span"));
         $name.text(name);
         $elements.push($name);
-        var $arguments = $(document.createElement("span"));
         $arguments.addClass("outline-entry-php-arg");
         $arguments.text(args);
         $elements.push($arguments);
@@ -32,27 +32,75 @@ define(function (require, exports, module) {
      * @returns {Array}   List of outline entries.
      */
     function getOutlineList(documentContent, showArguments, showUnnamed) {
-        var regex = /((\w*)\s*[=:]\s*)?((public|protected|private)\s*)?function(\s*|\s+\w*\s*)(\([\w,\s&$='"\\()]*\))/g;
-        var result = [];
-        documentContent.split("\n").forEach(function (line, index) {
-            var match = regex.exec(line);
-            while (match !== null) {
-                var vis = match[4] || defaultVisibilty;
-                var name = (match[5] || "").trim();
-                var args = showArguments ? match[6] : "";
-                match = regex.exec(line);
-                if (name.length === 0) {
-                    if (showUnnamed) {
-                        name = unnamedPlaceholder;
-                        vis = "unnamed";
-                    } else {
-                        continue;
-                    }
+        var re,
+            matchesLnr = [],
+            indCls = 0,
+            indFun = 0,
+            lCls = 0,
+            lFun = 0,
+            outline = [],
+            outlinesFun = [],
+            outlinesCls = [],
+            list = [];
+
+        re = /\n[\t ]*(final)?[\t ]*(public|protected|private)?[\t ]*(static)?[\t ]*function[\t ]+(\w*)[\t ]*(\([\w,\s&$='"\\()]*\))\s*\{/g;
+        documentContent.replace(re, function (match, p1, p2, p3, p4, p5, offset) {
+            outline = {name: p4, offset: offset, match: match};
+            outline.args = p5;
+            outline.visibility = p2 ||  defaultVisibilty;
+            outlinesFun.push(outline);
+        });
+
+        re = /\n[\t ]*class\s+(\w*)\s*\{/g;
+        documentContent.replace(re, function (match, p1, offset) {
+            outline = {name: p1, offset: offset, match: match};
+            outlinesCls.push(outline);
+        });
+
+        re = /\n/g;
+        documentContent.replace(re, function (match, offset) {
+            matchesLnr.push(offset);
+        });
+        lCls = outlinesCls.length;
+        lFun = outlinesFun.length;
+        matchesLnr.forEach(function (offset, index) {
+            for (indFun; indFun < lFun; indFun += 1) {
+                if (outlinesFun[indFun].offset < offset) {
+                    outlinesFun[indFun].line = index;
+                } else {
+                    break;
                 }
-                result.push(createListEntry(name, args, vis, index, line.length));
+            }
+            for (indCls; indCls < lCls; indCls += 1) {
+                if (outlinesCls[indCls].offset < offset) {
+                    outlinesCls[indCls].line = index;
+                } else {
+                    break;
+                }
             }
         });
-        return result;
+
+        list = [];
+        outlinesFun.forEach(function (outline, index) {
+            var name = outline.name,
+                args = (showArguments && outline.args) ? outline.args : '',
+                vis = outline.visibility || defaultVisibilty,
+                line = outline.line,
+                ch = 0;
+            list.push(createListEntry(name, args, vis, line, ch));
+        });
+        outlinesCls.forEach(function (outline, index) {
+            var name = outline.name,
+                args = '',
+                vis = 'class',
+                line = outline.line,
+                ch = 0;
+            list.push(createListEntry(name, args, vis, line, ch));
+        });
+        list.sort(function (a, b) {
+            return a.line - b.line;
+        });
+        return list;
     }
 
     function compare(a, b) {
